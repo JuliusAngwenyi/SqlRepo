a5f7ebceb594ccce6d6f82e3457ce96ef5da2fb0
Julius A <julius.bichage@gmail.com>
2021-02-13 23:11:00 +0000

    Create ShrinkAlDatabases.sql
    Script for shrinking all your databases in your SQL Server instance

---
+++ ShrinkAlDatabases.sql
@@ -0,0 +1,28 @@
+CREATE TABLE #DataBases (ID INT IDENTITY, Name NVARCHAR(100))
+
+INSERT #DataBases
+SELECT NAME FROM sys.databases WHERE NAME NOT IN ('master','model','msdb','tempdb')
+
+DECLARE @Count INT = 1
+DECLARE @NrOfDBs INT = 0
+
+SELECT @NrOfDBs = COUNT(0) FROM #DataBases
+
+DECLARE @DBName NVARCHAR(100), @SQL NVARCHAR(MAX)
+
+WHILE (@Count < @NrOfDBs)
+BEGIN
+     SELECT @DBName = Name FROM #DataBases WHERE ID = @Count
+
+     SELECT @SQL = 'ALTER DATABASE [' + @DBName + '] SET RECOVERY SIMPLE'
+
+     PRINT(@SQL)
+     EXEC(@SQL)
+
+     --Shrink Database
+     DBCC SHRINKDATABASE (@DBName , 0)
+     
+     SET @Count = @Count + 1
+END
+
+DROP TABLE #DataBases
